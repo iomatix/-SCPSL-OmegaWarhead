f037a31612987cf646dbe90ae8c735b97aec6ef9
iomatix <mateuszwypchlak@gmail.com>
2024-08-23 21:55:52 +0200

    Experimental

--- BetterOmegaWarhead/EventHandlers.cs
+++ BetterOmegaWarhead/EventHandlers.cs
@@ -37,16 +37,20 @@
             {
                 ev.IsAllowed = _plugin.Config.isStopAllowed;
             }
-            else if (!_plugin.Methods.isOmegaActivated() && 
-                ((Generator.List.Count(generator => (generator.State & GeneratorState.Engaged) == GeneratorState.Engaged) >= _plugin.Config.generatorsNumGuaranteeOmega) || (float)Loader.Random.NextDouble() * 100 < _plugin.Config.ReplaceAlphaChance)
-                )
+            else if (!_plugin.Methods.isOmegaActivated())
             {
-                float realTimeToDetonation = _plugin.Config.TimeToDetonation;
-                float eventTimeToDetonation = realTimeToDetonation - 0.45f; //- (1.5f * 10.0f);
-                _plugin.Methods.ActivateOmegaWarhead(eventTimeToDetonation);
-                ev.IsAllowed = _plugin.Config.isStopAllowed;
-                Warhead.Status = WarheadStatus.InProgress;
-                Warhead.DetonationTimer = realTimeToDetonation;
+                int engagedCount = Generator.List.Count(generator =>
+                       (generator.State & GeneratorState.Engaged) == GeneratorState.Engaged);
+                if (engagedCount >= _plugin.Config.generatorsNumGuaranteeOmega ||
+                    (float)Loader.Random.NextDouble() * 100 < _plugin.Config.ReplaceAlphaChance)
+                {
+                    float realTimeToDetonation = _plugin.Config.TimeToDetonation;
+                    float eventTimeToDetonation = realTimeToDetonation - 0.45f; //- (1.5f * 10.0f);
+                    _plugin.Methods.ActivateOmegaWarhead(eventTimeToDetonation);
+                    ev.IsAllowed = _plugin.Config.isStopAllowed;
+                    Warhead.Status = WarheadStatus.InProgress;
+                    Warhead.DetonationTimer = realTimeToDetonation;
+                }
             }
 
         }
