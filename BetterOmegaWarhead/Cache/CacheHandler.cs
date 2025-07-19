d0868bd7c0ac509bae45ab8360295185af02d0e4
iomatix <mateuszwypchlak@gmail.com>
2025-07-20 01:00:56 +0200

    CacheHandler refactor to LabAPI

--- BetterOmegaWarhead/Cache/CacheHandler.cs
+++ BetterOmegaWarhead/Cache/CacheHandler.cs
@@ -1,8 +1,8 @@
 ﻿namespace BetterOmegaWarhead
 {
-    using Exiled.API.Enums;
-    using Exiled.API.Extensions;
-    using Exiled.API.Features;
+    using BetterOmegaWarhead.Core.LoggingUtils;
+    using LabApi.Features.Wrappers;
+    using MapGeneration;
     using PlayerRoles;
     using System;
     using System.Collections.Generic;
@@ -24,10 +24,10 @@
             {
                 if (_cachedShelterLocations == null)
                 {
-                    Log.Debug("Initializing CachedShelterLocations via CacheShelterLocations.");
+                    LogHelper.Debug("Initializing CachedShelterLocations via CacheShelterLocations.");
                     _cachedShelterLocations = CacheShelterLocations();
                 }
-                Log.Debug($"Returning CachedShelterLocations with {_cachedShelterLocations.Count} locations.");
+                LogHelper.Debug($"Returning CachedShelterLocations with {_cachedShelterLocations.Count} locations.");
                 return _cachedShelterLocations;
             }
         }
@@ -39,10 +39,10 @@
             {
                 if (_cachedHeliSurvivors == null)
                 {
-                    Log.Debug("Initializing CachedHeliSurvivors as empty set.");
+                    LogHelper.Debug("Initializing CachedHeliSurvivors as empty set.");
                     _cachedHeliSurvivors = new HashSet<Player>();
                 }
-                Log.Debug($"Returning CachedHeliSurvivors with {_cachedHeliSurvivors.Count} players.");
+                LogHelper.Debug($"Returning CachedHeliSurvivors with {_cachedHeliSurvivors.Count} players.");
                 return _cachedHeliSurvivors;
             }
         }
@@ -54,66 +54,66 @@
             {
                 if (_cachedDisabledFactions == null)
                 {
-                    Log.Debug("Initializing CachedDisabledFactions as empty set.");
+                    LogHelper.Debug("Initializing CachedDisabledFactions as empty set.");
                     _cachedDisabledFactions = new HashSet<Faction>();
                 }
-                Log.Debug($"Returning CachedDisabledFactions with {_cachedDisabledFactions.Count} factions.");
+                LogHelper.Debug($"Returning CachedDisabledFactions with {_cachedDisabledFactions.Count} factions.");
                 return _cachedDisabledFactions;
             }
         }
 
         public HashSet<Vector3> GetCachedShelterLocations()
         {
-            Log.Debug("GetCachedShelterLocations called.");
+            LogHelper.Debug("GetCachedShelterLocations called.");
             return CachedShelterLocations;
         }
 
         public HashSet<Vector3> CacheShelterLocations()
         {
-            Log.Debug("CacheShelterLocations called, scanning for EzShelter rooms.");
+            LogHelper.Debug("CacheShelterLocations called, scanning for EzShelter rooms.");
             var shelterLocations = new HashSet<Vector3>();
             foreach (Room room in Room.List)
             {
-                if (room.Type == RoomType.EzShelter)
+                if (room.Name == RoomName.EzEvacShelter)
                 {
                     shelterLocations.Add(room.Position);
-                    Log.Debug($"Added shelter location: {room.Position}");
+                    LogHelper.Debug($"Added shelter location: {room.Position}");
                 }
             }
-            Log.Debug($"Cached {shelterLocations.Count} shelter locations.");
+            LogHelper.Debug($"Cached {shelterLocations.Count} shelter locations.");
             return shelterLocations;
         }
 
         public void CachePlayerEvacuatedByHelicopter(Player evacuatedPlayer)
         {
-            Log.Debug($"Caching player {evacuatedPlayer.Nickname} as evacuated by helicopter.");
+            LogHelper.Debug($"Caching player {evacuatedPlayer.Nickname} as evacuated by helicopter.");
             CachedHeliSurvivors.Add(evacuatedPlayer);
-            Log.Debug($"CachedHeliSurvivors now contains {CachedHeliSurvivors.Count} players.");
+            LogHelper.Debug($"CachedHeliSurvivors now contains {CachedHeliSurvivors.Count} players.");
         }
 
         public HashSet<Player> GetCachedPlayersEvacuatedByHelicopters()
         {
-            Log.Debug($"GetCachedPlayersEvacuatedByHelicopters called, returning {CachedHeliSurvivors.Count} players.");
+            LogHelper.Debug($"GetCachedPlayersEvacuatedByHelicopters called, returning {CachedHeliSurvivors.Count} players.");
             return CachedHeliSurvivors;
         }
 
         public bool IsPlayerEvacuatedByHelicopters(Player player)
         {
             bool isEvacuated = CachedHeliSurvivors.Contains(player);
-            Log.Debug($"Checking if {player.Nickname} is evacuated by helicopter: {isEvacuated}");
+            LogHelper.Debug($"Checking if {player.Nickname} is evacuated by helicopter: {isEvacuated}");
             return isEvacuated;
         }
 
         public HashSet<Faction> GetCachedDisabledFactions()
         {
-            Log.Debug($"GetCachedDisabledFactions called, returning {CachedDisabledFactions.Count} factions.");
+            LogHelper.Debug($"GetCachedDisabledFactions called, returning {CachedDisabledFactions.Count} factions.");
             return CachedDisabledFactions;
         }
 
         public bool IsFactionDisabled(Faction faction)
         {
             bool isDisabled = CachedDisabledFactions.Contains(faction);
-            Log.Debug($"Checking if faction {faction} is disabled: {isDisabled}");
+            LogHelper.Debug($"Checking if faction {faction} is disabled: {isDisabled}");
             return isDisabled;
         }
 
@@ -124,27 +124,27 @@
 
         public void ResetCache()
         {
-            Log.Debug("ResetCache called, clearing all caches.");
+            LogHelper.Debug("ResetCache called, clearing all caches.");
             _cachedShelterLocations = null;
-            Log.Debug("Cleared _cachedShelterLocations.");
+            LogHelper.Debug("Cleared _cachedShelterLocations.");
 
             if (_cachedHeliSurvivors != null)
             {
-                Log.Debug($"Disabling god mode for {_cachedHeliSurvivors.Count} evacuated players.");
+                LogHelper.Debug($"Disabling god mode for {_cachedHeliSurvivors.Count} evacuated players.");
                 foreach (Player player in _cachedHeliSurvivors)
                 {
                     player.IsGodModeEnabled = false;
-                    Log.Debug($"Disabled god mode for {player.Nickname}.");
+                    LogHelper.Debug($"Disabled god mode for {player.Nickname}.");
                 }
                 _cachedHeliSurvivors = null;
-                Log.Debug("Cleared _cachedHeliSurvivors.");
+                LogHelper.Debug("Cleared _cachedHeliSurvivors.");
             }
 
             if (_cachedDisabledFactions != null)
             {
-                Log.Debug($"Clearing {_cachedDisabledFactions.Count} disabled factions.");
+                LogHelper.Debug($"Clearing {_cachedDisabledFactions.Count} disabled factions.");
                 _cachedDisabledFactions = null;
-                Log.Debug("Cleared _cachedDisabledFactions.");
+                LogHelper.Debug("Cleared _cachedDisabledFactions.");
             }
         }
     }
