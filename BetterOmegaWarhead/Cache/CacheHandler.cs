260f78f1a0efe28d41404b7f7042bfdac6d82dbc
iomatix <mateuszwypchlak@gmail.com>
2025-07-20 13:07:55 +0200

    Documenting update

--- BetterOmegaWarhead/Cache/CacheHandler.cs
+++ BetterOmegaWarhead/Cache/CacheHandler.cs
@@ -8,66 +8,48 @@
     using System.Collections.Generic;
     using UnityEngine;
 
+    /// <summary>
+    /// Handles caching of runtime data for the BetterOmegaWarhead plugin,
+    /// including shelter locations, players evacuated by helicopter, and disabled factions.
+    /// </summary>
     public class CacheHandler
     {
-        private readonly Plugin _plugin;
-        public CacheHandler(Plugin plugin) => _plugin = plugin;
+        #region Fields
 
+        private readonly Plugin _plugin;
         private HashSet<Vector3> _cachedShelterLocations;
         private HashSet<Player> _cachedHeliSurvivors;
         private HashSet<Faction> _cachedDisabledFactions;
 
-        // Lazy initialization of shelter locations using a property getter.
-        private HashSet<Vector3> CachedShelterLocations
-        {
-            get
-            {
-                if (_cachedShelterLocations == null)
-                {
-                    LogHelper.Debug("Initializing CachedShelterLocations via CacheShelterLocations.");
-                    _cachedShelterLocations = CacheShelterLocations();
-                }
-                LogHelper.Debug($"Returning CachedShelterLocations with {_cachedShelterLocations.Count} locations.");
-                return _cachedShelterLocations;
-            }
-        }
+        #endregion
 
-        // Lazy initialization of heli survivors using a property getter.
-        private HashSet<Player> CachedHeliSurvivors
-        {
-            get
-            {
-                if (_cachedHeliSurvivors == null)
-                {
-                    LogHelper.Debug("Initializing CachedHeliSurvivors as empty set.");
-                    _cachedHeliSurvivors = new HashSet<Player>();
-                }
-                LogHelper.Debug($"Returning CachedHeliSurvivors with {_cachedHeliSurvivors.Count} players.");
-                return _cachedHeliSurvivors;
-            }
-        }
+        #region Constructor
 
-        // Lazy initialization of disabled factions.
-        private HashSet<Faction> CachedDisabledFactions
-        {
-            get
-            {
-                if (_cachedDisabledFactions == null)
-                {
-                    LogHelper.Debug("Initializing CachedDisabledFactions as empty set.");
-                    _cachedDisabledFactions = new HashSet<Faction>();
-                }
-                LogHelper.Debug($"Returning CachedDisabledFactions with {_cachedDisabledFactions.Count} factions.");
-                return _cachedDisabledFactions;
-            }
-        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CacheHandler"/> class.
+        /// </summary>
+        /// <param name="plugin">The plugin instance that owns this cache handler.</param>
+        public CacheHandler(Plugin plugin) => _plugin = plugin;
 
-        public HashSet<Vector3> GetCachedShelterLocations()
-        {
-            LogHelper.Debug("GetCachedShelterLocations called.");
-            return CachedShelterLocations;
-        }
+        #endregion
 
+        #region Shelter Caching
+
+        /// <summary>
+        /// Gets the cached list of EzEvacShelter locations.
+        /// If the cache has not been initialized, it will be created on first access.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="HashSet{Vector3}"/> containing the positions of all EzEvacShelter rooms.
+        /// </returns>
+        public HashSet<Vector3> GetCachedShelterLocations() => CachedShelterLocations;
+
+        /// <summary>
+        /// Scans the map and collects positions of all rooms with the <see cref="RoomName.EzEvacShelter"/> designation.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="HashSet{Vector3}"/> of shelter room positions.
+        /// </returns>
         public HashSet<Vector3> CacheShelterLocations()
         {
             LogHelper.Debug("CacheShelterLocations called, scanning for EzShelter rooms.");
@@ -84,6 +66,29 @@
             return shelterLocations;
         }
 
+        private HashSet<Vector3> CachedShelterLocations
+        {
+            get
+            {
+                if (_cachedShelterLocations == null)
+                {
+                    LogHelper.Debug("Initializing CachedShelterLocations via CacheShelterLocations.");
+                    _cachedShelterLocations = CacheShelterLocations();
+                }
+                LogHelper.Debug($"Returning CachedShelterLocations with {_cachedShelterLocations.Count} locations.");
+                return _cachedShelterLocations;
+            }
+        }
+
+        #endregion
+
+        #region Helicopter Evacuation Caching
+
+        /// <summary>
+        /// Adds a player to the list of helicopter-evacuated survivors.
+        /// This also implicitly initializes the evacuation cache if necessary.
+        /// </summary>
+        /// <param name="evacuatedPlayer">The <see cref="Player"/> instance to cache.</param>
         public void CachePlayerEvacuatedByHelicopter(Player evacuatedPlayer)
         {
             LogHelper.Debug($"Caching player {evacuatedPlayer.Nickname} as evacuated by helicopter.");
@@ -91,12 +96,23 @@
             LogHelper.Debug($"CachedHeliSurvivors now contains {CachedHeliSurvivors.Count} players.");
         }
 
+        /// <summary>
+        /// Gets the cached list of players who were successfully evacuated by helicopter.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="HashSet{Player}"/> of helicopter-evacuated players.
+        /// </returns>
         public HashSet<Player> GetCachedPlayersEvacuatedByHelicopters()
         {
             LogHelper.Debug($"GetCachedPlayersEvacuatedByHelicopters called, returning {CachedHeliSurvivors.Count} players.");
             return CachedHeliSurvivors;
         }
 
+        /// <summary>
+        /// Checks if a specific player is marked as having evacuated via helicopter.
+        /// </summary>
+        /// <param name="player">The <see cref="Player"/> instance to check.</param>
+        /// <returns><c>true</c> if the player is cached as evacuated; otherwise, <c>false</c>.</returns>
         public bool IsPlayerEvacuatedByHelicopters(Player player)
         {
             bool isEvacuated = CachedHeliSurvivors.Contains(player);
@@ -104,12 +120,41 @@
             return isEvacuated;
         }
 
+        private HashSet<Player> CachedHeliSurvivors
+        {
+            get
+            {
+                if (_cachedHeliSurvivors == null)
+                {
+                    LogHelper.Debug("Initializing CachedHeliSurvivors as empty set.");
+                    _cachedHeliSurvivors = new HashSet<Player>();
+                }
+                LogHelper.Debug($"Returning CachedHeliSurvivors with {_cachedHeliSurvivors.Count} players.");
+                return _cachedHeliSurvivors;
+            }
+        }
+
+        #endregion
+
+        #region Disabled Factions Caching
+
+        /// <summary>
+        /// Gets the cached list of factions that have been marked as disabled.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="HashSet{Faction}"/> representing disabled factions.
+        /// </returns>
         public HashSet<Faction> GetCachedDisabledFactions()
         {
             LogHelper.Debug($"GetCachedDisabledFactions called, returning {CachedDisabledFactions.Count} factions.");
             return CachedDisabledFactions;
         }
 
+        /// <summary>
+        /// Checks if the specified faction has been cached as disabled.
+        /// </summary>
+        /// <param name="faction">The <see cref="Faction"/> to check.</param>
+        /// <returns><c>true</c> if the faction is disabled; otherwise, <c>false</c>.</returns>
         public bool IsFactionDisabled(Faction faction)
         {
             bool isDisabled = CachedDisabledFactions.Contains(faction);
@@ -117,11 +162,37 @@
             return isDisabled;
         }
 
+        /// <summary>
+        /// Adds a faction to the cache of disabled factions.
+        /// </summary>
+        /// <param name="faction">The <see cref="Faction"/> to disable.</param>
         public void CacheDisabledFaction(Faction faction)
         {
             CachedDisabledFactions.Add(faction);
         }
 
+        private HashSet<Faction> CachedDisabledFactions
+        {
+            get
+            {
+                if (_cachedDisabledFactions == null)
+                {
+                    LogHelper.Debug("Initializing CachedDisabledFactions as empty set.");
+                    _cachedDisabledFactions = new HashSet<Faction>();
+                }
+                LogHelper.Debug($"Returning CachedDisabledFactions with {_cachedDisabledFactions.Count} factions.");
+                return _cachedDisabledFactions;
+            }
+        }
+
+        #endregion
+
+        #region Cache Reset
+
+        /// <summary>
+        /// Clears all cached data, including shelter locations, evacuated players, and disabled factions.
+        /// Also disables god mode on all helicopter-evacuated players.
+        /// </summary>
         public void ResetCache()
         {
             LogHelper.Debug("ResetCache called, clearing all caches.");
@@ -147,5 +218,7 @@
                 LogHelper.Debug("Cleared _cachedDisabledFactions.");
             }
         }
+
+        #endregion
     }
 }
--- BetterOmegaWarhead/Core/PlayerUtils/PlayerMethods.cs
+++ BetterOmegaWarhead/Core/PlayerUtils/PlayerMethods.cs
@@ -20,14 +20,20 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
     {
         private readonly Plugin _plugin;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlayerMethods"/> class.
+        /// </summary>
+        /// <param name="plugin">Reference to the core plugin instance.</param>
         public PlayerMethods(Plugin plugin)
         {
             _plugin = plugin;
         }
 
-        /// <summary>  
-        /// Generic escape handler that can process different escape scenarios.  
-        /// </summary>  
+        /// <summary>
+        /// Handles a generic escape sequence defined by an <see cref="EscapeScenario"/>.
+        /// </summary>
+        /// <param name="scenario">The escape scenario to handle.</param>
+        /// <returns>An enumerator for coroutine execution.</returns>
         public IEnumerator<float> HandleEscapeSequence(EscapeScenario scenario)
         {
             LogHelper.Debug($"{scenario.Name} escape sequence started.");
@@ -52,9 +58,10 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             LogHelper.Debug($"{scenario.Name} escape sequence completed.");
         }
 
-        /// <summary>  
-        /// Helicopter escape using the generic escape system.  
-        /// </summary>  
+        /// <summary>
+        /// Handles the helicopter escape sequence using the generic escape system.
+        /// </summary>
+        /// <returns>An enumerator for coroutine execution.</returns>
         public IEnumerator<float> HandleHelicopterEscape()
         {
             var helicopterScenario = new EscapeScenario
@@ -77,9 +84,11 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             return HandleEscapeSequence(helicopterScenario);
         }
 
-        /// <summary>  
-        /// Generic player escape processing.  
-        /// </summary>  
+        /// <summary>
+        /// Processes the escape logic for a single player.
+        /// </summary>
+        /// <param name="player">The player to process.</param>
+        /// <param name="scenario">The escape scenario defining the behavior.</param>
         private void ProcessPlayerEscape(Player player, EscapeScenario scenario)
         {
             if (!PlayerUtility.IsEligibleForEscape(player, scenario.EscapeZone, _plugin.Config.EscapeZoneSize))
@@ -98,9 +107,11 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             });
         }
 
-        /// <summary>  
-        /// Applies standard escape effects to a player.  
-        /// </summary>  
+        /// <summary>
+        /// Applies teleportation, effects, and hint message during player escape.
+        /// </summary>
+        /// <param name="player">The escaping player.</param>
+        /// <param name="scenario">The scenario context.</param>
         private void ApplyEscapeEffects(Player player, EscapeScenario scenario)
         {
             player.IsGodModeEnabled = true;
@@ -111,9 +122,9 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             player.EnableEffect<Ensnared>(duration: 30f);
         }
 
-        /// <summary>  
-        /// Disables all spawnable factions except None.  
-        /// </summary>  
+        /// <summary>
+        /// Disables all enemy faction spawns (e.g., NTF, Chaos) to prevent reinforcement after an escape event.
+        /// </summary>
         public void DisableFactionSpawn()
         {
             LogHelper.Debug("DisableFactionSpawn called.");
@@ -159,9 +170,9 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             LogHelper.Debug("DisableFactionSpawn completed.");
         }
 
-        /// <summary>  
-        /// Handles player fate on Omega Warhead detonation.  
-        /// </summary>  
+        /// <summary>
+        /// Evaluates and applies consequences to all players when the Omega Warhead detonates.
+        /// </summary>
         public void HandlePlayersOnNuke()
         {
             LogHelper.Debug("HandlePlayersOnNuke called.");
@@ -175,9 +186,11 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             LogHelper.Debug("HandlePlayersOnNuke completed.");
         }
 
-        /// <summary>  
-        /// Determines a player's fate during nuke detonation.  
-        /// </summary>  
+        /// <summary>
+        /// Determines whether the player is saved or killed during the nuke event.
+        /// </summary>
+        /// <param name="player">The player to evaluate.</param>
+        /// <returns>The fate of the player.</returns>
         private PlayerFate DeterminePlayerFate(Player player)
         {
             bool isInShelter = PlayerUtility.IsInShelter(player, _plugin.Config.ShelterZoneSize);
@@ -188,9 +201,11 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             return (isInShelter || isEvacuated) ? PlayerFate.Saved : PlayerFate.Killed;
         }
 
-        /// <summary>  
-        /// Processes the determined fate for a player.  
-        /// </summary>  
+        /// <summary>
+        /// Applies the result of a player's fate based on the determined value.
+        /// </summary>
+        /// <param name="player">The affected player.</param>
+        /// <param name="fate">The fate of the player (Saved/Killed).</param>
         private void ProcessPlayerFate(Player player, PlayerFate fate)
         {
             switch (fate)
@@ -207,9 +222,10 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             }
         }
 
-        /// <summary>  
-        /// Applies nuke exposure effects and kills the player.  
-        /// </summary>  
+        /// <summary>
+        /// Applies audio-visual effects to simulate nuke exposure and kills the player.
+        /// </summary>
+        /// <param name="player">The player to affect.</param>
         private void ApplyNukeEffects(Player player)
         {
             player.EnableEffect<Flashed>(duration: 0.75f);
@@ -219,9 +235,11 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
             player.Kill("Omega Warhead");
         }
 
-        /// <summary>  
-        /// Coroutine that temporarily saves a player from death by applying effects and God Mode.  
-        /// </summary>  
+        /// <summary>
+        /// Coroutine that visually affects and briefly saves the player from death.
+        /// </summary>
+        /// <param name="player">The player to save.</param>
+        /// <returns>An enumerator for coroutine execution.</returns>
         private IEnumerator<float> HandleSavePlayer(Player player)
         {
             LogHelper.Debug($"HandleSavePlayer coroutine started for {player.Nickname}.");
@@ -240,9 +258,9 @@ namespace BetterOmegaWarhead.Core.PlayerUtils
         }
     }
 
-    /// <summary>  
-    /// Represents possible player fates during events.  
-    /// </summary>  
+    /// <summary>
+    /// Represents the outcome of a player during a critical event.
+    /// </summary>
     public enum PlayerFate
     {
         Saved,
